a2a2b56d3a92c383580aecb2956998981c53af94
clintforster <clinte.forster@gmail.com>
2019-02-04 18:29:05 -0800

    Updates

---
+++ precache-manifest.ad65e9a6eff22f8081d56322ff8ca661.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "2ccf3f3936b5430f31d4",
+    "url": "/clicky-game/static/js/main.2ccf3f39.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "2ccf3f3936b5430f31d4",
+    "url": "/clicky-game/static/css/main.b4ce38c9.chunk.css"
+  },
+  {
+    "revision": "78aabb66d1c850a16e0a83efd354964b",
+    "url": "/clicky-game/index.html"
+  }
+];
